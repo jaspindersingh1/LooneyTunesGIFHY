9a4be3d5e72656138c6678ea49c15feb9b52ee39
Jaspinder Singh <jaspinders31@gmail.com>
2018-11-02 11:44:21 -0400

    reverted back to the original code

--- main.js
+++ main.js
@@ -6,11 +6,27 @@
 // api key = 7kIYFwakapwX5tKnveS4JHkXnDD53Zc3
 
 // buttons to be created from this array
-    var topics = ["Bugs Bunny", "Daffy Duff", "Tasmanian" ]
+var topics = ["Bugs Bunny", "Daffy Duff", "Tasmanian Devil" ]
+
+// create buttons from var topics and put in a function
+// function renderButtons() {
+    for (var i =0; i < topics.length; i++){
+        // console.log(topics.length)
+        // create a new button
+        var newButton = $("<button>")
+        newButton.addClass("cartoonButton");
+        // give the new button an attribute of its index
+        newButton.attr("data-name", topics[i])
+        // give the button its text
+        newButton.text(topics[i])
+        // console.log(newButton)
+        $("#buttonAppear").append(newButton)
+    }
+// }
 
 // connect to gifhy api
-    // $(".cartoonButton").on("click", function(){
-        function displayCartoons() {
+    $(".cartoonButton").on("click", function(){
+        // function displayCartoons() {
             var looneys = $(this).attr("data-name");
             var queryURL = "https:api.giphy.com/v1/gifs/search?q=" + looneys + "&api_key=7kIYFwakapwX5tKnveS4JHkXnDD53Zc3&limit=10";
 
@@ -37,24 +53,9 @@
                     $("#gifs-appear-here").prepend(looneyImg);
                 }
             });
-        }
-    // });
+        // }
+    });
 
-// create buttons from var topics and put in a function
-function renderButtons() {
-    for (var i =0; i < topics.length; i++){
-        // console.log(topics.length)
-        // create a new button
-        var newButton = $("<button>")
-        newButton.addClass("cartoonButton");
-        // give the new button an attribute of its index
-        newButton.attr("data-name", topics[i])
-        // give the button its text
-        newButton.text(topics[i])
-        // console.log(newButton)
-        $("#buttonAppear").append(newButton)
-    }
-}
 
 // function to add new cartoons from user
     $("#add-looney").on("click", function (event){
@@ -63,25 +64,26 @@ function renderButtons() {
         // grab the input from the searchbox
         var newLooney = $("#search-term").val().trim();
         // add search term to a new button and append to html
-        // var looneyButton = $("<button>")
-        // // console.log(looneyButton);
-        // looneyButton.addClass("cartoonButton");
-        // // give the new button an attribute of its index
-        // looneyButton.attr("data-name", newLooney)
-        // // give the button its text
-        // looneyButton.text(newLooney)
-        // // console.log(newButton)
-        // $("#buttonAppear").append(looneyButton)
-        // console.log(newLooney);
+        var looneyButton = $("<button>")
+        // console.log(looneyButton);
+        looneyButton.addClass("cartoonButton");
+        // give the new button an attribute of its index
+        looneyButton.attr("data-name", newLooney)
+        // give the button its text
+        looneyButton.text(newLooney)
+        // console.log(newButton)
+        $("#buttonAppear").append(looneyButton)
+        console.log(newLooney);
         // push new search to the topics array 
         topics.push(newLooney);
-        // console.log(topics)
+        console.log(topics)
 
-        renderButtons();
+        // renderButtons();
     });
 
-    // Adding a click event listener to all elements with a class of "movie-btn"
-    $(document).on("click", ".cartoonButton", displayCartoons);
+    // Adding a click event listener to all elements with a class of "cartoonButton"
+    $(document).on("click", ".cartoonButton", function(){
+    })
 
     // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+    // renderButtons();
